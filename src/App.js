73317e97af514c0e002b3790f5e5c65b5b451c27
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-04 15:38:07 +0300

    CartsMiddlePart is updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import FirstFooter from './Components/FirstFooter/FirstFooter';
 import { FirstHeader } from './Components/FirstHeader/FirstHeader';
+import CartsMiddlePart from './Components/CartsMiddlePart/CartsMiddlePart';
 
 function App() {
   return (
     <div className="App">
       <FirstHeader/>
+      <CartsMiddlePart />
       <FirstFooter/>
+      
     </div>
   );
 }
