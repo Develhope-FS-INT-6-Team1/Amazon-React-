f93cc5235ecd4e630e5207bad91f859ea2870ec6
Olala <olasabbouh@gmail.com>
2023-06-15 16:25:43 +0300

    fixing some iports and converting app.js index.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
+import React from 'react';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage.tsx';
-import PurchasePage from './Pages/PurchasePage/PurchasePage';
+import PurchasePage from './Pages/PurchasePage.tsx';
 import { SignInPage } from './Pages/SignInPage.tsx';
 import { SignUpPage } from './Pages/SignUpPage.tsx';
 import CartPage from './Pages/CartPage.tsx';
 import { SignInPage2 } from './Pages/SignInPage2.tsx';
+
 
 function App() {
   return (
@@ -18,9 +21,8 @@ function App() {
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/signin2" element={<SignInPage2 />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/cart" element={<CartPage />} />
+          <Route path="/cart" element={<CartPage CartItems={[]}/>} />
         </Routes>
-
       </div>
     </Router>
   );
--- src/Pages/PurchasePage.tsx
+++ src/Pages/PurchasePage.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import '../App.css';
-import FirstFooter from '../Components/FirstFooter/FirstFooter';
-import FirstHeader from '../Components/FirstHeader/FirstHeader';
-import EndFooter from '../Components/EndFooter/EndFooter';
-import PurchaseCard from '../Components/PurchaseCard/PurchaseCard';
-import './PurchasePage.css';
+import React, { useState } from "react";
+import FirstFooter from "../Components/FirstFooter/FirstFooter.tsx";
+import FirstHeader from "../Components/FirstHeader/FirstHeader.tsx";
+import EndFooter from "../Components/EndFooter/EndFooter.tsx";
+import "../Components/PurchaseCard/PurchasePage.css";
+import { ITEMS__DATA } from "../Assets/Data/data";
+import PurchaseCard from "../Components/PurchaseCard/PurchaseCard.tsx";
+import { useParams } from "react-router-dom";
 
-const PurchasePage: React.FC = (): JSX.Element => {
+interface Item {
+  id: number;
+  // Add more properties based on your data structure
+}
+
+function PurchasePage() {
+  const { id } = useParams<{ id: string }>();
+  const [data, setData] = useState<Item[]>(ITEMS__DATA);
+  const productId = parseInt(id, 10); // Replace 'yourProductId' with the desired product ID
+
+  const product = data.find((item) => item.id === productId);
+
   return (
     <div id="Main">
       <div className="wrapper-header1">
         <FirstHeader />
-      </div>
-      <div className="content-pcard">
-        <PurchaseCard />
+        <div className="content-pcard">
+          <div>
+            {product ? (
+              <PurchaseCard item={product} key={product.id} />
+            ) : (
+              <p>Product not found</p>
+            )}
+          </div>
+        </div>
       </div>
       <div className="footer-container-pp">
         <FirstFooter />
