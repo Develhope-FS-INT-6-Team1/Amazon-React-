dd66a430490e7980573b30d818d8bc5ef295bb82
Aisha-Abdullahi <boyaaisha@gmail.com>
2023-06-15 16:04:06 +0300

    remove from cart

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import FirstHeader from '../Components/FirstHeader/FirstHeader.tsx';
 import CartItemCard from '../Components/ItemCard/ItemCard.tsx';
@@ -17,7 +17,23 @@ interface CartPageProps {
 }
 
 const CartPage: React.FC<CartPageProps> = ({ CartItems }: CartPageProps): JSX.Element => {
-  const cartItems: CartItem[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  // const cartItems: CartItem[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+  const [cartItems, setCartItems] = useState<CartItem[]>(JSON.parse(localStorage.getItem('cartItems') || '[]'));
+
+
+  const handleRemoveItem = (itemId: number) => {
+    // Remove the item from the cartItems array
+    const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
+  
+    // Update the cartItems in local storage
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+  
+    // Update the state or perform any additional logic as needed
+    // For example, if you are using state to manage the cartItems:
+    setCartItems(updatedCartItems);
+  };
+  
 
   return (
     <div id="Main" className="App">
@@ -27,7 +43,7 @@ const CartPage: React.FC<CartPageProps> = ({ CartItems }: CartPageProps): JSX.El
         <div>No Items</div>
       ) : (
         cartItems.map((item: CartItem, index: number) => (
-          <CartItemCard key={index} item={item} />
+          <CartItemCard key={index} item={item} onRemoveItem={handleRemoveItem}/>
         ))
       )}
       <FirstFooter />
