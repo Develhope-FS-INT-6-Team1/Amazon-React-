6ae11fdf4a887af28614badc1c118bffa88004b1
codingbyarshya <117349670+codingbyarshya@users.noreply.github.com>
2023-06-15 07:44:18 +0300

    HomePage.tsx

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -7,52 +7,50 @@ import { useEffect, useState } from 'react';
 import { SecondHeader } from '../Components/SecondHeader/SecondHeader.tsx';
 import userJson from '../Users.json';
 
-
-
+interface User {
+  userID: string;
+  userName: string;
+  preferedLanguage: string;
+  preferedCurrency: string;
+}
 
 function HomePage() {
-  const [userId, setUserId] = useState('');
-  const [userName, setUserName] = useState('');
-  const [preferedLanguage, setPreferedLanguage] = useState('');
-  const [preferedCurrency, setPreferedCurrency] = useState('');
+  const [userId, setUserId] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+  const [preferedLanguage, setPreferedLanguage] = useState<string>('');
+  const [preferedCurrency, setPreferedCurrency] = useState<string>('');
 
-
-  useEffect(()=> {
+  useEffect(() => {
     const fetchUserData = async () => {
       try {
         const value = await localStorage.getItem('userId');
-        if(value != null){
+        if (value !== null) {
           console.log(value);
           setUserId(value);
-          
         }
 
-          
-        const user = userJson.find((user) => user.userID === userId);
+        const user = userJson.find((user: User) => user.userID === userId);
         if (user) {
-          console.log("User has logged in");
+          console.log('User has logged in');
           setUserName(user.userName);
           setPreferedLanguage(user.preferedLanguage);
           setPreferedCurrency(user.preferedCurrency);
         }
-        
-        
-      } 
-      catch (error) {
+      } catch (error) {
         console.error('Error fetching user data:', error);
       }
     };
+
     fetchUserData();
   }, [userId]);
 
   return (
     <div id="Main" className="App">
-      <FirstHeader/>
-      <SecondHeader/>
+      <FirstHeader />
+      <SecondHeader />
       <CartsMiddlePart />
-      <FirstFooter/>
-      <EndFooter/>
-      
+      <FirstFooter />
+      <EndFooter />
     </div>
   );
 }
