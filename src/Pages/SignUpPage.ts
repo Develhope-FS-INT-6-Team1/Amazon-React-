7929cb130d295c09cd60dcc071c5fde093f60a22
codingbyarshya <117349670+codingbyarshya@users.noreply.github.com>
2023-06-15 07:40:10 +0300

    SignUpPage.tsx

--- src/Pages/SignUpPage.ts
+++ src/Pages/SignUpPage.ts
@@ -1,11 +1,10 @@
 import '../App.css';
 import { SignUp } from '../Components/SignUp/SignUp.tsx';
 
-
 export function SignUpPage() {
     return (
         <div id="Main" className="App">
             <SignUp/>
         </div>
-      );
+    );
 }
