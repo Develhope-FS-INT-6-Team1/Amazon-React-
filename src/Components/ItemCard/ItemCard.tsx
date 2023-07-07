a33d9e9d14af0da9f4c4ff7af4ea88d7ffa223e2
Aisha-Abdullahi <boyaaisha@gmail.com>
2023-07-08 02:34:39 +0300

    cart page fix

--- src/Components/ItemCard/ItemCard.tsx
+++ src/Components/ItemCard/ItemCard.tsx
@@ -11,12 +11,15 @@ interface CartItem {
 interface CartItemCardProps {
   item: CartItem;
   onRemoveItem: (itemId: number) => void;
+  onChangeQuantity: (quantity: number) => void; 
 }
 
-const CartItemCard: React.FC<CartItemCardProps> = ({ item, onRemoveItem }) => {
+const CartItemCard: React.FC<CartItemCardProps> = ({ item, onRemoveItem, onChangeQuantity }) => {
   const [quantity, setQuantity] = useState<number>(item.quantity);
 
-  const handleChangeQuantity = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChangeQuantity = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const { value } = event.target;
     setQuantity(Number(value));
   };
@@ -30,108 +33,64 @@ const CartItemCard: React.FC<CartItemCardProps> = ({ item, onRemoveItem }) => {
   };
 
   return (
-    <div className="item-wrap">
-      <div className="item-and-checkout">
-        <div className="cart-item">
-          <div className="-row">
-            <div className="title">Shopping Cart</div>
-            <div className="price">Price</div>
-          </div>
-          <hr></hr>
-          <div className="-row">
-            <img src={item.image} alt="" className="item-image" />
-            <div className="item-details">
-              <div className="item-description">
-              <p>{item.description}</p>
-              <p>In Stock</p>
-              <p className="body-text">
-                <label className="body-text">
-                  <input type="checkbox" name="" value="" className="body-text" />
-                  <span className="body-text">
-                    This is a gift
-                    <span className="body-text">
-                      <a href="#">&nbsp;Learn more</a>
-                    </span>
-                  </span>
-                </label>
-              </p>
-              </div>
-              
-              <div className="quantity">
-                <select
-                  name="quantity"
-                  tabindex="0"
-                  className="quantity-dropdown-menu"
-                  type="number"
-                  min="1"
-                  value={quantity}
-                  onChange={handleChangeQuantity}
-                >
-                  <option value="0">0 </option>
-                  <option value="1" selected="">
-                    1
-                  </option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10+</option>
-                </select>
-
-                <p className="cart-links">
-                                    <span>|</span> &nbsp; &nbsp; &nbsp;
-                                    <a onClick={handleRemoveItem}>Delete</a> &nbsp; &nbsp; &nbsp;
-                                    <span>|</span> &nbsp; &nbsp; &nbsp;
-                                    <a href="#">Save for later</a> &nbsp; &nbsp; &nbsp;
-                                    <span> | </span> &nbsp; &nbsp; &nbsp;
-                                    <a href="#">Compare with similar items</a> &nbsp; &nbsp; &nbsp;
-                                    <span> | </span> &nbsp; &nbsp; &nbsp;
-                                    <a href="#">Share</a> &nbsp; &nbsp; &nbsp;
-                                </p>
-
-              </div>
-            </div>
-            <div className="item-price">{item.price}</div>
-          </div>
-          <div className="-row-rem">
-            <div className="subtotal">Subtotal ({quantity} Items): <span className="subtotal-amount"> {calculateSubtotal()}</span>
-            </div>
-          </div>
-          <hr />
-        </div>
-
-        <div className="policy">
-          <div className="checkout">
-            <div className="subtotal">Subtotal ({quantity} Items):
-              <span className="subtotal-amount">
-                {calculateSubtotal()}
+    <div className="cart-holder">
+      <hr style={{ border: 'none', height: '1px', backgroundColor: 'black' }} />
+      <img src={item.image} alt="" className="item-image" />
+      <div className="item-details">
+        <div className="item-description">
+          <p>{item.description}</p>
+          <div className="item-price">{item.price}</div>
+          <p>In Stock</p>
+          <p className="body-text">
+            <label className="body-text">
+              <input type="checkbox" name="" value="" className="body-text" />
+              <span className="body-text">
+                This is a gift
+                <span className="body-text">
+                  <a href="#">&nbsp;Learn more</a>
+                </span>
               </span>
-            </div>
-            <div>
-              <input type="checkbox" id="gift" />
-              <label htmlFor="gift">This order contains a gift</label>
-            </div>
-            <button id="checkout-button">Proceed to checkout</button>
-          </div>
+            </label>
+          </p>
         </div>
 
+        <div className="quantity">
+          <select
+            name="quantity"
+            tabindex="0"
+            className="quantity-dropdown-menu"
+            type="number"
+            min="1"
+            value={quantity}
+            onChange={handleChangeQuantity}
+          >
+            <option value="0">0 </option>
+            <option value="1" selected="">
+              1
+            </option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10+</option>
+          </select>
+
+          <p className="cart-links">
+            <a onClick={handleRemoveItem}>Delete</a> &nbsp; &nbsp; &nbsp;
+            <span>|</span> &nbsp; &nbsp;
+            <a href="#">Save for later</a> &nbsp; &nbsp; &nbsp;
+            <span> | </span> &nbsp; &nbsp;
+            <a href="#">Compare with similar items</a> &nbsp; &nbsp; &nbsp;
+            <span> | </span> &nbsp; &nbsp; &nbsp;
+            <a href="#">Share</a> &nbsp; &nbsp; &nbsp;
+          </p>
+        </div>
       </div>
-      <div style={{height:'60px',width:'73%', background:'white', marginTop:'20px', marginBottom:'40px'}}>
-      </div>
-        <p className="bottom-paragraph">
-          The price and availability of items at Amazon.com are subject to
-          change. The Cart is a temporary place to store a list of your items
-          and reflects each item's most recent price. <a href="/">Learn more</a>
-          <br /> Do you have a gift card or promotional code? We'll ask you to
-          enter your claim code when it's time to pay.
-        </p>
-        
     </div>
-    
   );
 };
 
--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,55 +1,117 @@
-import React, {useState} from 'react';
-import '../App.css';
-import FirstHeader from '../Components/FirstHeader/FirstHeader.tsx';
-import CartItemCard from '../Components/ItemCard/ItemCard.tsx';
-import FirstFooter from '../Components/FirstFooter/FirstFooter.tsx';
-import EndFooter from '../Components/EndFooter/EndFooter.tsx';
-import { SecondHeader } from '../Components/SecondHeader/SecondHeader.tsx';
+import React, { useState } from "react";
+import "../App.css";
+import FirstHeader from "../Components/FirstHeader/FirstHeader.tsx";
+import CartItemCard from "../Components/ItemCard/ItemCard.tsx";
+import FirstFooter from "../Components/FirstFooter/FirstFooter.tsx";
+import EndFooter from "../Components/EndFooter/EndFooter.tsx";
+import { SecondHeader } from "../Components/SecondHeader/SecondHeader.tsx";
+import "../Components/ItemCard/ItemCard.css";
 
 interface CartItem {
   id: number;
   name: string;
   price: number;
+  quantity: number; // Added quantity property
 }
 
 interface CartPageProps {
   CartItems: CartItem[];
 }
 
-const CartPage: React.FC<CartPageProps> = ({ CartItems }: CartPageProps): JSX.Element => {
+const CartPage: React.FC<CartPageProps> = ({
+  CartItems, // Corrected the destructured prop name
+}: CartPageProps): JSX.Element => {
   // const cartItems: CartItem[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
 
-  const [cartItems, setCartItems] = useState<CartItem[]>(JSON.parse(localStorage.getItem('cartItems') || '[]'));
-
+  const [cartItems, setCartItems] = useState<CartItem[]>(
+    JSON.parse(localStorage.getItem("cartItems") || "[]")
+  );
 
   const handleRemoveItem = (itemId: number) => {
-    // Remove the item from the cartItems array
     const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
-  
-    // Update the cartItems in local storage
-    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
-  
-    // Update the state or perform any additional logic as needed
-    // For example, if you are using state to manage the cartItems:
+    localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
     setCartItems(updatedCartItems);
   };
-  
+
+  const handleChangeQuantity = (itemId: number, newQuantity: number) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  const calculateSubtotal = () => {
+    let subtotal = 0;
+    for (const item of cartItems) {
+      subtotal += item.price * item.quantity;
+    }
+    return subtotal;
+  };
 
   return (
     <div id="Main" className="App">
       <FirstHeader />
       <SecondHeader />
-      {cartItems.length === 0 ? (
-        <div>No Items</div>
-      ) : (
-        cartItems.map((item: CartItem, index: number) => (
-          <CartItemCard key={index} item={item} onRemoveItem={handleRemoveItem}/>
-        ))
-      )}
+      <div className="item-wrap">
+        <div className="-row"></div>
+        <div className="item-and-checkout">
+          <div className="cart-item">
+            <div className="-row">
+              <div className="title">Shopping Cart</div>
+              {/* <div className="price">Price</div> */}
+            </div>
+            <hr style={{ border: 'none', height: '1px', backgroundColor: 'black' }} />
+            {cartItems.length === 0 ? (
+              <div>No Items</div>
+            ) : (
+              cartItems.map((item: CartItem, index: number) => (
+                <CartItemCard
+                  key={index}
+                  item={item}
+                  onRemoveItem={handleRemoveItem}
+                />
+              ))
+            )}
+          </div>
+          <div className="policy">
+            <div className="checkout">
+              <div className="subtotal">
+                Subtotal ({cartItems.length} Items):{" "}
+                <span className="subtotal-amount">{calculateSubtotal()}</span>
+              </div>
+              <div>
+                <input type="checkbox" id="gift" />
+                <label htmlFor="gift">This order contains a gift</label>
+              </div>
+              <button id="checkout-button">Proceed to checkout</button>
+            </div>
+          </div>
+          <div
+            style={{
+              height: '60px',
+              width: '73%',
+              background: 'white',
+              marginTop: '20px',
+              marginBottom: '40px',
+            }}
+          ></div>
+          <p className="bottom-paragraph">
+            The price and availability of items at Amazon.com are subject to
+            change. The Cart is a temporary place to store a list of your items
+            and reflects each item's most recent price.{" "}
+            <a href="/">Learn more</a>
+            <br /> Do you have a gift card or promotional code? We'll ask you to
+            enter your claim code when it's time to pay.
+          </p>
+        </div>
+      </div>
       <FirstFooter />
       <EndFooter />
     </div>
   );
-}
+};
 
 export default CartPage;
