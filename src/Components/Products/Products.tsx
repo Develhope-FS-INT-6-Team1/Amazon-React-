8a6b6383b7082994b5c69037f2e299a451e56041
Omer <hsynmrgry@gmail.com>
2023-07-08 21:06:00 +0300

    products page by OLA <3

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -25,11 +25,69 @@ interface ProductsProp {
 export default function Products(){
 
     const [products, setProducts] = useState<Item[]>([]);
-    const [priceFilter, setPriceFilter] = useState("");
+    const [priceFilter, setPriceFilter] = useState("none");
 
     useEffect(() => {
       fetchProducts();
-    }, []);
+     /* setTimeout(()=>{
+        let newProducts : any[] = [];
+      for(let i = 0 ; i < products.length;i++){
+        let temp : any = products[i];
+        temp.price = temp.price.slice(1);
+        newProducts.push(temp);
+        console.log(temp);
+      }
+      
+      setProducts(newProducts);
+
+
+
+      },300);
+      */
+
+    }, [priceFilter]);
+
+
+    const filteredProducts = () =>{
+        let newProducts : any[] = [];
+        for(let i = 0 ; i < products.length;i++){
+            let temp : any = products[i];
+            if(temp.price.charAt(0) == '$')
+                temp.price = temp.price.slice(1);
+            if(priceFilter == 'none'){
+                newProducts.push(temp);
+                console.log(temp);
+
+            }
+            else if(priceFilter == 'under25'){
+                if(Number(temp.price) < 25){
+                    newProducts.push(temp);
+                    console.log(temp);
+                }
+            }
+            else if(priceFilter == '25to50'){
+                if(Number(temp.price) >= 25 && Number(temp.price) < 50){
+                    newProducts.push(temp);
+                    console.log(temp);
+                }
+            }
+            else if(priceFilter == '50to100'){
+                if(Number(temp.price) >= 50 && Number(temp.price) < 100){
+                    newProducts.push(temp);
+                    console.log(temp);
+                }
+            }
+            else if(priceFilter == '100above'){
+                if(Number(temp.price) >= 100){
+                    newProducts.push(temp);
+                    console.log(temp);
+                }
+            }
+        }
+        return newProducts;
+    }
+
+
   
     const fetchProducts = async () => {
       await axios.get('http://localhost:3001/products')
@@ -51,10 +109,10 @@ export default function Products(){
             <div>
                 <p>1-24 of over 30 results</p>
             </div>
+
             <div className="filters-results">
                 <div className="filters">
                     <h3>Filters</h3>
-                    <div className="filter-by">
                         <h4>By Price:</h4>
                         <p>
                             <input
@@ -62,6 +120,7 @@ export default function Products(){
                                 value="All"
                                 checked={priceFilter === "none"}
                                 onChange={() => handlePriceFilterChange("none")}
+                                
                             />
                             All
                         </p>
@@ -101,60 +160,23 @@ export default function Products(){
                             />
                             $100 & Above
                         </p>
-                    </div>
-                    <div className="filter-by">
-                        <h4>By Rate:</h4>
-                        <p><input type="radio"/>1 & up</p>
-                        <p><input type="radio"/>2 & up</p>
-                        <p><input type="radio"/>3 & up</p>
-                        <p><input type="radio"/>4 & up</p>
-                    </div>
-                    <div className="filter-by">
-                        <h4>By Department:</h4>
-                        <p><input type="radio"/>Arts & Crafts</p>
-                        <p><input type="radio"/>Automotive</p>
-                        <p><input type="radio"/>Baby</p>
-                        <p><input type="radio"/>Beauty & Personal Care</p>
-                        <p><input type="radio"/>Books</p>
-                        <p><input type="radio"/>Boy's Fashion</p>
-                        <p><input type="radio"/>Computers</p>
-                        <p><input type="radio"/>Deals</p>
-                        <p><input type="radio"/>Digital Music</p>
-                        <p><input type="radio"/>Electronics</p>
-                        <p><input type="radio"/>Girl's Fashion</p>
-                        <p><input type="radio"/>Health & Household</p>
-                        <p><input type="radio"/>Home & Kitchen</p>
-                        <p><input type="radio"/>Industrial & Scientific</p>
-                        <p><input type="radio"/>Kindle Store</p>
-                        <p><input type="radio"/>Luggage</p>
-                        <p><input type="radio"/>Men's Fashion</p>
-                        <p><input type="radio"/>Music & TV</p>
-                        <p><input type="radio"/>Music, CDs & Vinyl</p>
-                        <p><input type="radio"/>Pet Supplies</p>
-                        <p><input type="radio"/>Prime Videos</p>
-                        <p><input type="radio"/>Software</p>
-                        <p><input type="radio"/>Sports & Outdoor</p>
-                        <p><input type="radio"/>Tools & Home Improvements</p>
-                        <p><input type="radio"/>Toys & Games</p>
-                    </div>
                 </div>
+
                 <div className="results">
-                    <div><h3>Results</h3></div>
-                    <div className="products-grid">
-                        <div className="product-box">
-                            {products.map((item) => (
-                                <div key={item.productid} className="product-item">
-                                    <Link to={`/purchase/${item.productid}`}>
-                                        <img src={item.image} height="200px" alt="product" />
-                                    </Link>
-                                    <div className="product-details">
-                                        <h5>{item.name}</h5>
-                                        <p>{item.description}</p>
-                                        <p>Price: ${item.price}</p>
-                                    </div>
+                    <h3>Results</h3>
+                    <div className="product-box">
+                        {filteredProducts().map((item) => (
+                            <div key={item.productid} className="product-item">
+                                <Link to={`/purchase/${item.productid}`}>
+                                    <img src={item.image} height="200px" alt="product" />
+                                </Link>
+                                <div className="product-details">
+                                    <h5>{item.productname}</h5>
+                                    <p>{item.aboutthisitem}</p>
+                                    <p>Price: ${item.price}</p>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
