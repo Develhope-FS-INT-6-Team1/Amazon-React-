8e225358851afa75ccbe9f7612f43ccc7ff1b098
Olala <olasabbouh@gmail.com>
2023-07-09 23:28:55 +0300

    homepage responsive

--- src/Components/CartsMiddlePart/CartsMiddlePart.tsx
+++ src/Components/CartsMiddlePart/CartsMiddlePart.tsx
@@ -161,6 +161,7 @@ export default function CartsMiddlePart() {
                     src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_D2_45M_en_US_2x._CB418309979_.jpg"
                     height="242px"
                     alt=""
+                    className="img-hide"
                   />
                 </div>
               </div>
